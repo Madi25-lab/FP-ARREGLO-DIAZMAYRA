f84ceee22312d0b2a9270f7eb055c4358282f210
Madi25-lab <Madic2507@gmail.com>
2025-06-20 12:21:24 -0500

    Agregando ejercicio 6 version final para la suma de numeros pares e impares en los numeros de un vector

---
+++ Ejercicio6.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int n;
+	int i=0;
+	int pares=0, impares=0;
+
+	cout<< "Ingrese la cantidad de numeros que tendra el vector: ";
+	cin>>n;
+	if (n>i){
+	int numeros[n];
+	
+	cout<<endl;
+	for (int i=0; i<n; i++){
+		cout<<i+1<< ". Ingrese un numero:" ;
+		cin>> numeros[i];
+		}
+	}else {
+		cout<<"Numero invalido";
+	}
+	cout<<endl;
+	
+	int numeros[n];
+	
+	for (int i=2; i<n; i++){
+		if(numeros[i]%2==0){
+			pares+= numeros[i];
+		}else{
+			impares+= numeros[i];
+		}
+	}
+	
+	cout<< "La suma de numeros pares es: "<<pares<<endl;
+	cout<< "La suma de numeros impares es: "<<impares<<endl;
+	
+	return 0;
+}
+	
+	
+	
