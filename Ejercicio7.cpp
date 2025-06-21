5796c10097043a979c0aefc8626ebad0bea8c08d
Madi25-lab <Madic2507@gmail.com>
2025-06-21 11:56:47 -0500

    Agregando ejercicio 7, version final para invertir la lista de un vector

--- Ejercicio7.cpp
+++ Ejercicio7.cpp
@@ -1,21 +1,46 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int n, i, aux;
-	int numeros[n];
+	int i ,n, aux;
 	
-	cout<<"Ingrese la cantidad de numeros en el vector"<<endl;
+	cout<<"Ingrese la cantidad de numeros que contendra el vector: ";
 	cin>>n;
 	
-	for(i=0; i<n; i++){
-		cout<<i+1<<" Ingrese un numero: "<<endl;
-		cin>>numeros[i];
-	}
+	cout<<endl;
+	int numeros[n];
+    for (i=0; i<n; i++){
+		cout<<i+1<< ". Ingrese un numero:" ;
+		cin>> numeros[i];
+		}
+	cout<<endl;
 	
-	cout<< "Los numeros ingresados en el vector son"<< endl;
-	for(int i=0;i<n;i++){
-	cout<<"["<<numeros[i]<<"]";
+	cout<< "El vector actual de numeros es: "<< endl;
+	cout<<"[";
+	for (i=0; i<n; i++){
+	cout<<numeros[i];
+	if(i<n-1){
+		cout<<";";
+	    }
     }
+     cout<<"]";
+     
+    cout<<endl;
+    for (i=0; i<n/2; i++){
+    	aux=numeros[i];
+    	numeros[i]=numeros[n-1-i];
+    	numeros[n-1-i]=aux;
+    }
+	cout<<endl;
+	
+	cout<< "El vector inverso es: "<< endl;
+	cout<<"[";
+	for (i=0; i<n; i++){
+	cout<<numeros[i];
+	if(i<n-1){
+		cout<<";";
+	    }
+    }
+	cout<<"]";
 	
 	return 0;
 }
