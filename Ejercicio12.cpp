a5b8eff35ea15c18b0173d5b434d961feb2af0e6
Madi25-lab <Madic2507@gmail.com>
2025-06-22 12:09:03 -0500

    Agregando ejercicio 12 version final, con la implementacion de una funcion para el reconocimiento de que este ordenado o no

--- Ejercicio12.cpp
+++ Ejercicio12.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 using namespace std;
+
+bool VectOrdenado(int numeros[], int n){
+	for (int i = 0; i < n-1; i++) {
+        if (numeros[i] > numeros[i + 1]) {
+            return false; 
+        }
+    }
+    return true; 
+}
+
 int main(){
 	int n,i=0;
 	bool ordenado;
@@ -19,6 +29,14 @@ int main(){
 	}while(i < n);
 	
 	cout<<endl;
+	
+	ordenado=VectOrdenado(numeros,n);
+	
+	if(ordenado){
+		cout<<"El arreglo se encuentra ordenado"<<endl;
+	}else{
+		cout<<"El arreglo no se encuentra ordenado"<<endl;
+	}
 	
 	return 0;
 }
