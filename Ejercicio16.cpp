361fcababac8b70cfe438711a2f35c9e415745d2
Madi25-lab <Madic2507@gmail.com>
2025-06-23 09:50:40 -0500

    Agregando ejercicio 16, version final para la validacion de solo numeros y este la integracion de mas 2

--- Ejercicio16.cpp
+++ Ejercicio16.cpp
@@ -11,9 +11,21 @@ int main(){
 	cout<<"Digite un enunciado: ";
 	cin.getline(enunciado,100);
 	
-
 	int i=0,j=0;
 	while(enunciado[i] != '\0'){
-		
+	if(isdigit(enunciado[i])){ 
+		numeros[j] = enunciado[i];  
+		j++;                    
+		}
+		i++;						
+	}
+	numeros[j] = '\0';                
+	
+	
+	NFinal = atoi(numeros);
+	NFinal = NFinal+2;
+	
+	cout<<"El numero final es: "<<endl;
+	cout<<NFinal;
 	return 0;
 }
