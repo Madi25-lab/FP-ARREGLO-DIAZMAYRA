c12d13c9816d2a6f874a8eadc73def913e1de323
Madi25-lab <Madic2507@gmail.com>
2025-06-23 12:46:23 -0500

    Agregando ejercicio 20 version final, encontrando las ventas minimas, regulares y excelentes y porcentajes de estos

--- Ejercicio20.cpp
+++ Ejercicio20.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main (){
 	float Ventas[12];
-	float P;
+	float S=0,P;
 	int Ex=0, Re=0,Mi=0;
 	string mes[12]={"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 	
@@ -16,16 +16,37 @@ int main (){
 	for (int i = 0; i < 12; i++) {
         cout<<"Ingrese la venta mensual del mes de "<< mes[i] << ": ";
         cin >> Ventas[i];
-        P= P+Ventas[i];
+        S= S+Ventas[i];
     }
-	P=P/12.0;
+	P=S/12.0;
 	cout<<endl;
 	cout<<"El promedio de las ventas es: "<<P;
 	cout<<endl;
 	
+	float Minimo=P*0.4;
+	float Regular=P*0.75;
+	
+	cout<<endl;
+	cout<< "Venta excelente en los meses de: "<<endl;
+	for (int i = 0; i < 12; i++) {
+        if (Ventas[i]<Minimo){
+			Mi=Mi+Ventas[i];
+		}else if(Ventas[i]<=Regular){
+			Re=Re+Ventas[i];
+		}else{
+			cout<< mes[i]<<endl;
+			Ex=Ex+Ventas[i];
+		}
+    }
     
-	
-	
+    cout<<endl;
+    float PMi=Mi*100/S;
+    float PRe=Re*100/S;
+    float PEx=Ex*100/S;
+    cout<< "Porcentaje de meses con venta minima: "<<PMi<<"%"<<endl;
+    cout<< "Porcentaje de meses con venta minima: "<<PRe<<"%"<<endl;
+    cout<< "Porcentaje de meses con venta minima: "<<PEx<<"%"<<endl;
+
 	
 	return 0;
 }
