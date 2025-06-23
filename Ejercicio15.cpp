113a6decaa24e564b8bd6c065ed56278c3c8e3ae
Madi25-lab <Madic2507@gmail.com>
2025-06-23 01:25:34 -0500

    Agregando avance del ejercicio 15, recepcion de las palabras para la comparacion de estas

---
+++ Ejercicio15.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int mistrcmp(char str1, char str2) {
+     while ()){
+        
+
+
+int main(){
+	char str1[100],str2[100];
+	
+	cout<<"Ingrese una palabra para el primer arreglo: ";
+    cin>>str1;
+    cout<<"Ingrese una palabra para el segundo arreglo";
+    cin>>str2;
+    cout<<endl;
+    
+    mistrcmp(str1,str2);
+	
+	
+	return 0;
+}
+
+    
+ 
