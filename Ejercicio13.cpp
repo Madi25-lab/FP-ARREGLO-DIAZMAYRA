061057feeb11f8aabfc823acc96a59042eb5ad1d
Madi25-lab <Madic2507@gmail.com>
2025-06-22 23:54:41 -0500

    Agregando avance del ejercicio 13, para pedir las variables de str1 y str2

---
+++ Ejercicio13.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	char str1[100],str2[100];
+	cout<<"Ingrese str1: ";
+    cin>>str1;
+    cout<<"Ingrese str2: ";
+    cin>>str2;
+    cout<<endl;
+    
+	
+	return 0;
+}
+    
+ 
