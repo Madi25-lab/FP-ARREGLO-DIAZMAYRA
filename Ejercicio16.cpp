6f9b9e2595994f684fefa511c7bf4298e92fc2ea
Madi25-lab <Madic2507@gmail.com>
2025-06-23 09:04:19 -0500

    Agregando avance del ejercicio 16 peticion de datos

---
+++ Ejercicio16.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cctype>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+	int NFinal;
+	char enunciado[100];
+	char numeros[100];
+	
+	cout<<"Digite un enunciado: ";
+	cin.getline(enunciado,100);
+	
+
+	int i=0,j=0;
+	while(enunciado[i] != '\0'){
+		
+	return 0;
+}
