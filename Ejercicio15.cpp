5999a04ad578988dbd7760c7c68a6a04d9848e45
Madi25-lab <Madic2507@gmail.com>
2025-06-23 07:50:40 -0500

    Agregando avance 2 del ejercicio 15 implemetando una funcion para el reconocimiento de similitud entre 2 cadenas de palabra

--- Ejercicio15.cpp
+++ Ejercicio15.cpp
@@ -2,13 +2,31 @@
 
 using namespace std;
 
-int mistrcmp(char str1, char str2) {
-     while ()){
+int mistrcmp(char str1[], char str2[]) {
+    int i;
+	while(str1[i] != '\0' && str2[i] != '\0'){
+		if(str1[i] > str2[i]){
+			return 1;
+		}else if(str1[i] < str2[i]){
+			return -1;
+		}
+		i++;
+	}
+	
+	if(str1[i] == '\0' && str2[i] == '\0'){
+		return 0;
+	}else if(str1[i] == '\0'){
+		return -1;
+	}else{
+		return 1;
+	}
+
+} 
         
-
-
 int main(){
-	char str1[100],str2[100];
+	char str1[100];
+	int str2[100];
+	int Similitud;
 	
 	cout<<"Ingrese una palabra para el primer arreglo: ";
     cin>>str1;
@@ -16,8 +34,9 @@ int main(){
     cin>>str2;
     cout<<endl;
     
-    mistrcmp(str1,str2);
-	
+    Similitud = mistrcmp(str1, str2);
+    
+
 	
 	return 0;
 }
