074a1a97a31ab09da75fc898914adab0b900e15d
Madi25-lab <Madic2507@gmail.com>
2025-06-23 00:45:20 -0500

    Agregando avance del ejercicio 14, recepcion de palabras str1 y str2

---
+++ Ejercicio14.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+void mistrcat(char str1[], char str2[]){
+	
+
+int main(){
+	char str1[100];
+	char str2[100];
+	
+	cout<<"Ingrese la primera palabra: "; 
+	cin>>str1;
+	cout<<"Ingrese la segunda palabra: ";
+	cin>>str2;
+	
+	
+	return 0;
+}
+
